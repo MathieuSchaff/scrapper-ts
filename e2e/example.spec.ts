a53b960af8dd99a1678fe520623d0beb11c0d3fd
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-09 07:16:14 +0200

    setting an expect timeout for first test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
+import getRandomInt from "../app/utils/randomInt.ts";
+test.only('if search are on the document', async ({ page }) => {
 
-test('if search are on the document', async ({ page }) => {
-  await page.goto("https://www.welcometothejungle.com/en");
+  await page.goto("https://www.welcometothejungle.com/en/jobs?refinementList%5Boffices.country_code%5D%5B%5D=FR&query=react%20js&page=1")
   await page.mainFrame().waitForSelector("header");
   const jobLink = page.getByRole("link", {
     name: "Find a job",
@@ -12,15 +13,15 @@ test('if search are on the document', async ({ page }) => {
   const jobSearchButton = page.getByTestId("jobs-home-search-field-query");
   await jobSearchButton.waitFor({ state: "visible" });
   await jobSearchButton.focus();
-  expect(jobSearchButton).toBeAttached();
-  const clearLocationButton = page.getByLabel('Clear all')
-  await clearLocationButton.waitFor({ state: "visible" });
-  expect(clearLocationButton).toBeAttached();
+  await jobSearchButton.type("tests", {
+    delay: getRandomInt(100, 500),
+  });
+  await expect(jobSearchButton).toHaveValue("tests", { timeout: 10000 });
+  const clearLocationButton = page.getByLabel('Clear all').nth(1)
+  expect(clearLocationButton).toBeAttached({ timeout: 1000 });
   await clearLocationButton.click({ delay: 1000 });
   const locationInput = page.getByTestId('jobs-home-search-field-location')
-  expect(locationInput).toBeAttached();
-  expect(locationInput).toHaveAttribute('value', '')
-  await locationInput.press("Enter");
+  await expect(locationInput).toHaveAttribute('value', '', { timeout: 1000 });
 });
 
 test('if filters works', async ({ page }) => {
